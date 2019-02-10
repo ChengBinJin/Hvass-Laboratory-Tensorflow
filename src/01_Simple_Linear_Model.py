5d0da9b8fb3b236e0f1ffc8a7623e32da2805805
chengbin <sbkim0407@gmail.com>
2019-02-11 01:57:46 +0900

    add 01_Simple_Linear_Model.py

---
+++ src/01_Simple_Linear_Model.py
@@ -0,0 +1,251 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+from sklearn.metrics import confusion_matrix
+from mnist import MNIST
+
+print("Tensorflow version: {}".format(tf.__version__))
+data = MNIST()
+
+print("Size of:")
+print("- Training-set:\t\t{}".format(data.num_train))
+print("- Validation-set:\t{}".format(data.num_val))
+print("- Test-set:\t\t{}".format(data.num_test))
+
+# The images are stored in one-dimensional arrays of this length.
+img_size_flat = data.img_size_flat
+print('- img_size_flat:\t{}'.format(img_size_flat))
+
+# tuple with heigh and width of images used to reshape arrays.
+img_shape = data.img_shape
+print('- img_shape:\t\t{}'.format(img_shape))
+
+# Number of classes, one class for each of 10 digits.
+num_classes = data.num_classes
+print('- num_classes:\t\t{}'.format(num_classes))
+
+print(data.y_test[0:5, :])
+print(data.y_test_cls[0:5])
+
+# Helper-function for plotting images
+def plot_images(images, cls_true, cls_pred=None, pred_logits=None):
+    assert len(images) == len(cls_true) == 9
+
+    # Create figure with 3x3 sub-plots.
+    fig, axes = plt.subplots(3, 3)
+    fig.subplots_adjust(hspace=0.3, wspace=0.3)
+
+    for i, ax in enumerate(axes.flat):
+        # Plot image
+        ax.imshow(images[i].reshape(img_shape), cmap='binary')
+
+        # Show true and predicted classes.
+        if cls_pred is None:
+            xlabel = "True: {0}".format(cls_true[i])
+        else:
+            xlabel = "True: {0} Pred: {1} ({2}%)".format(cls_true[i], cls_pred[i], int(pred_logits[i]*100.))
+
+        ax.set_xlabel(xlabel)
+
+        # Remove ticks from the plot.
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+    # Ensure the plot is shown correctly with multiple plots in a single Notebook cell.
+    plt.show()
+
+# Plot a few images to see if data is correct
+# Get the first images from the test-set
+images = data.x_test[0:9]
+
+# Get the true classes for those images.
+cls_true = data.y_test_cls[0:9]
+
+# Plot the images and labels using our helper-function above.
+plot_images(images=images, cls_true=cls_true)
+
+# Placeholder variables
+x = tf.placeholder(tf.float32, [None, img_size_flat])
+y_true = tf.placeholder(tf.float32, [None, num_classes])
+y_true_cls = tf.placeholder(tf.int64, [None])
+
+# Variables to be optimized
+weights = tf.Variable(tf.zeros([img_size_flat, num_classes]))
+biases = tf.Variable(tf.zeros([num_classes]))
+
+# Model
+logits = tf.matmul(x, weights) + biases
+y_pred = tf.nn.softmax(logits)
+y_pred_cls = tf.argmax(y_pred, axis=1)
+y_pred_logits = tf.reduce_max(y_pred, axis=1)
+
+# Cost-function to be optimized
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=y_true)
+cost = tf.reduce_mean(cross_entropy)
+
+# Optimization method
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.5).minimize(cost)
+
+# Performance measure
+correct_prediction = tf.equal(y_pred_cls, y_true_cls)
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+# TensorFlow Run
+# Create TensorFlow session
+session = tf.Session()
+
+# Initialize variables
+session.run(tf.global_variables_initializer())
+
+# Helper-function to perform optimization iterations
+batch_size = 100
+
+def optimize(num_iterations):
+    for i in range(num_iterations):
+        # Get a batch of trainign examples.
+        # x_batch now holds a batch of iamges and
+        # y_true_batch are the true labels for those images.
+        x_batch, y_true_batch, _ = data.random_batch(batch_size=batch_size)
+
+        # Put the batch into a dict with the proper names
+        # for placholder variables in the TensorFlow graph.
+        # Note that the placeholder for y_true_cls is not set
+        # because it is not used during training.
+        feed_dict_train = {x: x_batch,
+                           y_true: y_true_batch}
+
+        # Run the optimizer using this batch of training data.
+        # TensorFlow assigns the variables in feed_dict_train
+        # to the placeholder variables and then runs the optimizer.
+        session.run(optimizer, feed_dict=feed_dict_train)
+
+# Helper-functions to show performance
+feed_dict_test = {x: data.x_test,
+                  y_true: data.y_test,
+                  y_true_cls: data.y_test_cls}
+
+def print_accuracy():
+    # Use TensorFlow to compute the accuracy.
+    acc = session.run(accuracy, feed_dict=feed_dict_test)
+
+    # Print the accuracy.
+    print("Accuracy on test-set: {0:.1%}".format(acc))
+
+def print_confusion_matrix():
+    # Get the true classifications for the test-set.
+    cls_true = data.y_test_cls
+
+    # Get the predicted classifications for the test-set.
+    cls_pred = session.run(y_pred_cls, feed_dict=feed_dict_test)
+
+    # Get the confusion matrix using sklearn.
+    cm = confusion_matrix(y_true=cls_true,
+                          y_pred=cls_pred)
+
+    # Print the confusion matrix as text.
+    print(cm)
+
+    # Plot the confusion matrix as an image.
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+
+    # Make various adjustments to the plot.
+    plt.tight_layout()
+    plt.colorbar()
+    tick_marks = np.arange(num_classes)
+    plt.xticks(tick_marks, range(num_classes))
+    plt.yticks(tick_marks, range(num_classes))
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
+
+    # Ensure the plot is shown correctly with multiple plots
+    # in a single Notebook cell.
+    plt.show()
+
+def plot_example_errors():
+    # Use TensorFlow to get a list of bollean values
+    # wether each test-image has been correctly classified,
+    # and a list for the predicted class of each image.
+    correct, cls_pred, logits_pred = session.run([correct_prediction, y_pred_cls, y_pred_logits],
+                                                 feed_dict=feed_dict_test)
+
+    # Negative the bollean array.
+    incorrect = (correct == False)
+
+    # Get the images from the test-set that have been
+    # incorrectly classified.
+    images = data.x_test[incorrect]
+
+    # Get the predicted classes for those images.
+    cls_pred = cls_pred[incorrect]
+
+    # Get the predicted logtis for those images.
+    logits_pred = logits_pred[incorrect]
+
+    # Get the true classes for those images.
+    cls_true = data.y_test_cls[incorrect]
+
+    # Plot the first 9 images.
+    plot_images(images=images[0:9],
+                cls_true=cls_true[0:9],
+                cls_pred=cls_pred[0:9],
+                pred_logits=logits_pred[0:9])
+
+# Helper-function to plot the model weights
+def plot_weights():
+    # Get the values for the weights from the TensorFlow variable.
+    w = session.run(weights)
+
+    # Get the lowest and highest values for the weights.
+    # This is used to correct the colour intensity across
+    # the images so they can be compared with each other.
+    w_min = np.min(w)
+    w_max = np.max(w)
+
+    # Create figure with 3x4 sub-plots,
+    # where the last 2 sub-plots are unused.
+    fig, axes = plt.subplots(3,4)
+    fig.subplots_adjust(hspace=0.3, wspace=0.3)
+
+    for i, ax in enumerate(axes.flat):
+        # Only use the weights for the first 10 sub-plots.
+        if i < 10:
+            # Get the weights for the i'th digit and reshape it.
+            # Note that w.shape == (img_size_flat, 10)
+            image = w[:, i].reshape(img_shape)
+
+            # Set the label for the sub-plot.
+            ax.set_xlabel("Weights: {0}".format(i))
+
+            # Plot the image.
+            ax.imshow(image, vmin=w_min, vmax=w_max, cmap='seismic')
+
+        # Remove ticks from each sub-plot.
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+    # Ensure the plot is shown correctly with multiple plots
+    # in a single Notebook cell.
+    plt.show()
+
+# # Performance before any optimization
+# print_accuracy()
+# plot_example_errors()
+#
+# # Performance after 1 optimization iteration
+# optimize(num_iterations=1)
+# print_accuracy()
+# plot_example_errors()
+# plot_weights()
+
+# Performance after 10 optimiztion iteration
+# optimize(num_iterations=10)
+# print_accuracy()
+# plot_example_errors()
+# plot_weights()
+
+# Performance after 1000 optimization iteration
+optimize(num_iterations=1000)
+print_accuracy()
+plot_example_errors()
+plot_weights()
+print_confusion_matrix()
