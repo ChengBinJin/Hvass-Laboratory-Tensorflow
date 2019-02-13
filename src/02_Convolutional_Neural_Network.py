02cf5b4636266f8dbf80d90a585e31d0e3f1f7e2
chengbin <sbkim0407@gmail.com>
2019-02-13 18:10:54 +0900

    finish 02_Convolutional_Neural_network.py

--- src/02_Convolutional_Neural_Network.py
+++ src/02_Convolutional_Neural_Network.py
@@ -47,8 +47,8 @@ num_channels = data.num_channels
 
 
 # Helper-function for plotting images
-def plot_images(images, cls_true, cls_pred=None):
-    assert len(images) == len(cls_true) == 9
+def plot_images(images_, cls_true_, cls_pred=None):
+    assert len(images_) == len(cls_true) == 9
 
     # Create figure with 3x3 sub-plots.
     fig, axes = plt.subplots(3, 3)
@@ -56,13 +56,13 @@ def plot_images(images, cls_true, cls_pred=None):
 
     for i, ax in enumerate(axes.flat):
         # plot image.
-        ax.imshow(images[i].reshape(img_shape), cmap='binary')
+        ax.imshow(images_[i].reshape(img_shape), cmap='binary')
 
         # Show true and predicted classes.
         if cls_pred is None:
-            xlabel = "True: {0}".format(cls_true[i])
+            xlabel = "True: {0}".format(cls_true_[i])
         else:
-            xlabel = "True: {0}, Pred: {1}".format(cls_true[i], cls_pred[i])
+            xlabel = "True: {0}, Pred: {1}".format(cls_true_[i], cls_pred[i])
 
         # Show the classes as the label on the x-axis.
         ax.set_xlabel(xlabel)
@@ -84,7 +84,7 @@ images = data.x_test[0:9]
 cls_true = data.y_test_cls[0:9]
 
 # Plot the images and labels using our helper-function above.
-plot_images(images=images, cls_true=cls_true)
+plot_images(images_=images, cls_true_=cls_true)
 
 
 # Helper-functions for creating new variables
@@ -97,7 +97,7 @@ def new_biases(length):
 
 
 # Helper-function for creating a new Convolutional Layer
-def new_conv_layer(input,               # The previous layer.
+def new_conv_layer(input_,               # The previous layer.
                    num_input_channels,  # Num, channels in prev. layer.
                    filter_size,         # Width and height of each filter.
                    num_filters,         # Number of filters.
@@ -122,7 +122,7 @@ def new_conv_layer(input,               # The previous layer.
     # is moved 2 pixels across the x- and y-axis of the image.
     # The padding is set to 'SAME' which means the input image
     # is padded with zeros so the size of the output is the same.
-    layer = tf.nn.conv2d(input=input,
+    layer = tf.nn.conv2d(input=input_,
                          filter=weights,
                          strides=[1, 1, 1, 1],
                          padding='SAME')
@@ -135,4 +135,457 @@ def new_conv_layer(input,               # The previous layer.
     if use_pooling:
         # This is 2x2 max-pooling, which means that we
         # consider 2x2 windows and select the largest value
-        # in each window. Then we mov
+        # in each window. Then we move 2 pixels to the next window.
+        layer = tf.nn.max_pool(value=layer,
+                               ksize=[1, 2, 2, 1],
+                               strides=[1, 2, 2, 1],
+                               padding='SAME')
+
+    # Rectified Linear Unit (ReLU).
+    # It calculates max(x, 0) for each input pixel x.
+    # This adds some non-linearity to the formula and allows us
+    # to learn more complicated functions.
+    layer = tf.nn.relu(layer)
+
+    # Note that ReLU is normally executed before the pooling,
+    # but since relu(max_pool(x)) == max_pool(relu(x)) we can
+    # save 75% of the relu-operations by max-pooling first.
+
+    # We return both the resulting layer and the first-weights
+    # because we will plot the weights layer.
+    return layer, weights
+
+
+# Helper-function for flattening a layer
+def flatten_layer(layer):
+    # Get the shape of the input layer.
+    layer_shape = layer.get_shape()
+
+    # The shape of the input layer is assumed to be:
+    # layer_shape == [num_images, img_height, img_width, num_channels]
+
+    # The number of feature is: img_height * img_width * num_channels
+    # We can use a function from TensorFlow to calculate this.
+    num_features_ = layer_shape[1:4].num_elements()
+
+    # Reshape the layer to [num_images, num_features].
+    # Note that we just set the size of the second dimension
+    # to num_features and the size of the first dimension to -1
+    # which means the size in that dimension is calculated
+    # so the total size of the tensor is unchanged from the reshaping.
+    layer_flat_ = tf.reshape(layer, [-1, num_features_])
+
+    # The shape of the flattened layer is now:
+    # [num_images, img_height * img_width * num_channels]
+
+    # Return both the flattened layer and the number of features.
+    return layer_flat_, num_features_
+
+
+def new_fc_layer(input_,             # The previous layer.
+                 num_inputs,        # Num. inputs from prev. layer.
+                 num_outputs,       # Num. outputs.
+                 use_relu=True):    # Use Rectified Linear Unit (ReLU)?
+    # Create new weights and biases.
+    weights = new_weights(shape=[num_inputs, num_outputs])
+    biases = new_biases(length=num_outputs)
+
+    # Calculate the layer as the matrix multiplication of
+    # the input and weights, and then add the bias-values.
+    layer = tf.matmul(input_, weights) + biases
+
+    # Use ReLU?
+    if use_relu:
+        layer = tf.nn.relu(layer)
+
+    return layer
+
+
+# Placeholder variables
+x = tf.placeholder(tf.float32, shape=[None, img_size_flat], name='x')
+x_image = tf.reshape(x, [-1, img_size, img_size, num_channels])
+y_true = tf.placeholder(tf.float32, shape=[None, num_classes], name='y_true')
+y_true_cls = tf.argmax(y_true, axis=1)
+
+# Convolutional Layer 1
+layer_conv1, weights_conv1 = new_conv_layer(input_=x_image,
+                                            num_input_channels=num_channels,
+                                            filter_size=filter_size1,
+                                            num_filters=num_filters1,
+                                            use_pooling=True)
+print('layer_conv1: {}'.format(layer_conv1))
+
+# Convolutional Layer 2
+layer_conv2, weights_conv2 = new_conv_layer(input_=layer_conv1,
+                                            num_input_channels=num_filters1,
+                                            filter_size=filter_size2,
+                                            num_filters=num_filters2,
+                                            use_pooling=True)
+print('layer_conv2: {}'.format(layer_conv2))
+
+# Flatten Layer
+layer_flat, num_features = flatten_layer(layer_conv2)
+print('layer_flat: {}'.format(layer_flat))
+print('num_featuers: {}'.format(num_features))
+
+# Fully-Connected layer 1
+layer_fc1 = new_fc_layer(input_=layer_flat,
+                         num_inputs=num_features,
+                         num_outputs=fc_size,
+                         use_relu=True)
+print('layer_fc1: {}'.format(layer_fc1))
+
+# Fully-Connected layer 2
+layer_fc2 = new_fc_layer(input_=layer_fc1,
+                         num_inputs=fc_size,
+                         num_outputs=num_classes,
+                         use_relu=False)
+print('layer_fc2: {}'.format(layer_fc2))
+
+# Predicted Class
+y_pred = tf.nn.softmax(layer_fc2)
+y_pred_cls = tf.argmax(y_pred, axis=1)
+
+# Cost-function to be optimized
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=layer_fc2,
+                                                           labels=y_true)
+cost = tf.reduce_mean(cross_entropy)
+
+# Optimization Method
+optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(cost)
+
+# Performance Meausres
+correct_prediction = tf.equal(y_pred_cls, y_true_cls)
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+# TensorFlow Run
+# Create TensorFlow session
+session = tf.Session()
+
+# Initialize variables
+session.run(tf.global_variables_initializer())
+
+# Helper-function to perform optimization iteractions
+train_batch_size = 64
+
+# Counter for total number of iterations performed so far.
+total_iterations = 0
+
+def optimize(num_iterations):
+    # Ensure we update the global variable rather than a local copy.
+    global total_iterations
+
+    # Start-time used for priting time-usage below.
+    start_time = time.time()
+
+    for i in range(total_iterations, total_iterations + num_iterations):
+        # Get a batch of training examples.
+        # x_batch now holds a batch of images and
+        # y_true_batch are the true labels for those images.
+        x_batch, y_true_batch, _ = data.random_batch(batch_size=train_batch_size)
+
+        # Put the batch into a dict with the proper naems
+        # for placeholder variables in the TensorFlow graph.
+        feed_dict_train = {x: x_batch,
+                           y_true: y_true_batch}
+
+        # Run the optimizer using this batch of training data.
+        # TensorFlow assigns the variables in feed_dict_train
+        # to the placeholder variables and then runs the optimizer.
+        session.run(optimizer, feed_dict=feed_dict_train)
+
+        # Print status every 100 iterations.
+        if i % 100 == 0:
+            # Calculate the accuracy on the training-set.
+            acc = session.run(accuracy, feed_dict=feed_dict_train)
+
+            # Message for printing.
+            msg = "Optimization Iteration: {0:>6,}, Training Accuracy: {1:>6.1%}"
+
+            # Print it.
+            print(msg.format(i+1, acc))
+
+    # Update the total number of iterations performed.
+    total_iterations += num_iterations
+
+    # Ending time.
+    end_time = time.time()
+
+    # Difference between start and end-times.
+    time_dif = end_time - start_time
+
+    # print the time-usage.
+    print("Time usage: " + str(timedelta(seconds=int(round(time_dif)))))
+
+
+# Helper-function to plot example errors
+def plot_example_erros(cls_pred, correct):
+    # This function is called from print_test_accuracy() below.
+
+    # cls_pred is an array of the predicted class-number for
+    # all images in the test-set.
+
+    # correct is a boolean array wheter the predicted class
+    # is equal to the true class for each image in the test-set.
+
+    # Negate the boolean array.
+    incorrect = (correct == False)
+
+    # Get the images from the test-set that have been
+    # incorrectly classified.
+    images_ = data.x_test[incorrect]
+
+    # Get the predicted classes for those images.
+    cls_pred = cls_pred[incorrect]
+
+    # Get the true class for those images.
+    cls_true_ = data.y_test_cls[incorrect]
+
+    # plot the first 9 images.
+    plot_images(images_=images_[0:9],
+                cls_true_=cls_true_[0:9],
+                cls_pred=cls_pred[0:9])
+
+
+# Helper-function to plot confusion matrix
+def plot_confusion_matrix(cls_pred):
+    # This is called from print_test_accuracy() below.
+
+    # cls_pred is an array of the predicted class-number for
+    # all images in the test-set.
+
+    # Get the true classification for the test-set.
+    cls_true_ = data.y_test_cls
+
+    # Get the confusion matrix using sklearn.
+    cm = confusion_matrix(y_true=cls_true_,
+                          y_pred=cls_pred)
+
+    # Print the confusion matrix as text.
+    print(cm)
+
+    # Plot the confusion matrix as an image.
+    plt.matshow(cm)
+
+    # Make various adjustments to the plot.
+    plt.colorbar()
+    tick_marks = np.arange(num_classes)
+    plt.xticks(tick_marks, range(num_classes))
+    plt.yticks(tick_marks, range(num_classes))
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
+
+    # Ensure the plot is shown correctly with multiple plots
+    # in a single Notebook cell.
+    plt.show()
+
+
+# Helper-function for showing the performance
+# Split the test-set into smaller batches of this size.
+test_batch_size= 256
+
+def print_test_accuracy(show_example_errors=False,
+                        show_confusion_matrix=False):
+    # Number of images in the test-set.
+    num_test = data.num_test
+
+    # Allocate an array for the predicted classes which
+    # will be calculated in batches and filled into this array.
+    cls_pred = np.zeros(shape=num_test, dtype=np.int)
+
+    # Now calculate the predicted classes for the batches.
+    # We will just iterate through all the batches.
+    # There might be a more clever and Pythonic way of doing this.
+
+    # The starting index for the next batch is denoted i.
+    i = 0
+
+    while i < num_test:
+        # The ending index for the next batch is denoted j.
+        j = min(i + test_batch_size, num_test)
+
+        # Get the images from the test-set between index i and j.
+        images_ = data.x_test[i:j, :]
+
+        # Get the associated labels.
+        labels = data.y_test[i:j, :]
+
+        # Create a feed-dict with these images and labels
+        feed_dict = {x: images_,
+                     y_true: labels}
+
+        # Calculuate the predicted class using TensorFlow.
+        cls_pred[i:j] = session.run(y_pred_cls, feed_dict=feed_dict)
+
+        # Set the start-index for the next batch to the
+        # end-index of the current batch.
+        i = j
+
+    # Convenience variable for the true class-numbers of the test-set.
+    cls_true_ = data.y_test_cls
+
+    # Create a boolean array whether each image is correctly classified.
+    correct = (cls_true_ == cls_pred)
+
+    # Calculate the number of correctly classified images.
+    # When summing a boolean array, False mean 0 and True means 1.
+    correct_sum = correct.sum()
+
+    # Classification accuracy is the number of correctly classified
+    # images divided by the total number of images in the test-set.
+    acc = float(correct_sum) / num_test
+
+    # Print the accuracy.
+    msg = "Accuracy on Test-Set: {0:.1%} ({1} / {2})"
+    print(msg.format(acc, correct_sum, num_test))
+
+    # Plot some examples of mis-classifications, if desired.
+    if show_example_errors:
+        print("Example errors:")
+        plot_example_erros(cls_pred=cls_pred, correct=correct)
+
+    # Plot the confusion matrix, if desired.
+    if show_confusion_matrix:
+        print("Confusion Matrix:")
+        plot_confusion_matrix(cls_pred=cls_pred)
+
+
+# Performance before any optimization
+print_test_accuracy()
+
+# Performance after 1 optimization iteration
+optimize(num_iterations=1)
+print_test_accuracy()
+
+# Performance after 100 optimization iterations
+optimize(num_iterations=99)  # We already performed 1 iteration above.
+print_test_accuracy(show_example_errors=True)
+
+# Performance after 1000 optimization iterations
+optimize(num_iterations=900)  # We performed 100 iterations above.
+print_test_accuracy(show_example_errors=True)
+
+# Performance after 10,000 optimization iterations
+optimize(num_iterations=9000)  # We performed 1000 iterations above.
+print_test_accuracy(show_example_errors=True,
+                    show_confusion_matrix=True)
+
+# Visualization of Weights and Layers
+# Helper-function for plotting convolutional wegiths
+def plot_conv_weights(weights, input_channel=0):
+    # Assume weights are TensorFlow ops for 4-dim variables
+    # e.g. weights_conv1 or weights_conv2
+
+    # Retrieve the values of the weight-variables from TensorFlow.
+    # A feed-dict is not necessary because nothing is calculated.
+    w = session.run(weights)
+
+    # Get the lowest and highest values for the weights.
+    # This is used to correct the colour intensity across
+    # the images so they can be compared with each other.
+    w_min = np.min(w)
+    w_max = np.max(w)
+
+    # Number of filters used in the conv. layer.
+    num_filters = w.shape[3]
+
+    # Number of grids to plot.
+    # Rounded-up, square-root of the number of filters.
+    num_grids = math.ceil(math.sqrt(num_filters))
+
+    # Create figure with a grid of sub-plots.
+    fig, axes = plt.subplots(num_grids, num_grids)
+
+    # Plot all the filter-weights.
+    for i, ax in enumerate(axes.flat):
+        # Only plot the valid filter-weights.
+        if i<num_filters:
+            # Get the weights for the i'th filter of the input channel.
+            # See new_conv_layer() for details on the format
+            # of this 4-dim tensor.
+            img = w[:, :, input_channel, i]
+
+            # Plot image.
+            ax.imshow(img, vmin=w_min, vmax=w_max,
+                      interpolation='nearest', cmap='seismic')
+
+        # Remove ticks from the plot.
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+    # Ensure the plot is shown correctly with multiple plots
+    # in a single Notebook cell.
+    plt.show()
+
+# Helper-function for plotting the output of a convolutional layer
+def plot_conv_layer(layer, image):
+    # Assume layer is a TensorFlow op that outputs a 4-dim tensor
+    # which is the output of a convolutional layer,
+    # e.g. layer_conv1 or layer_conv2.
+
+    # Create a feed-dict containing just one image.
+    # Note that we don't need to feed y_true because it is
+    # not used in this calculation.
+    feed_dict = {x: [image]}
+
+    # Calculate and retrieve the output values of the layer
+    # when inputting that image.
+    values = session.run(layer, feed_dict=feed_dict)
+
+    # Number of filters used in the conv. layer.
+    num_filters = values.shape[3]
+
+    # Number of grids to plot.
+    # Rounded-up, square-root of the number of filters.
+    num_grids = math.ceil(math.sqrt(num_filters))
+
+    # Create figure with a grid of sub=plots.
+    fig, axes = plt.subplots(num_grids, num_grids)
+
+    # Plot the output images of all the filters.
+    for i, ax in enumerate(axes.flat):
+        # Only plot the images for valid filters.
+        if i<num_filters:
+            # Get the output image of using the i'th filter.
+            # See new_conv_layer() for details on the format
+            # of this 4-dim tensor.
+            img = values[0, :, :, i]
+
+            # Plot image.
+            ax.imshow(img, interpolation='nearest', cmap='binary')
+
+        # Remove ticks from the plot.
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+    # Ensure the plot is shown correctly with multiple plots
+    # in a single Notebook ceil.
+    plt.show()
+
+
+# Input Images
+def plot_image(image):
+    plt.imshow(image.reshape(img_shape),
+               interpolation='nearest',
+               cmap='binary')
+    plt.show()
+
+
+image1 = data.x_test[0]
+plot_image(image1)
+
+image2 = data.x_test[13]
+plot_image(image2)
+
+# Convolution Layer 1
+plot_conv_weights(weights=weights_conv1)
+plot_conv_layer(layer=layer_conv1, image=image1)
+plot_conv_layer(layer=layer_conv1, image=image2)
+
+# Convolution Layer 2
+plot_conv_weights(weights=weights_conv2, input_channel=0)
+plot_conv_weights(weights=weights_conv2, input_channel=1)
+
+plot_conv_layer(layer=layer_conv2, image=image1)
+plot_conv_layer(layer=layer_conv2, image=image2)
+
