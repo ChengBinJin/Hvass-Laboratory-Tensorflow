e5f00fedd1cafef3184960ca7c0248e4697f38c1
chengbin <sbkim0407@gmail.com>
2019-03-07 23:31:54 +0900

    implementing 06_CIFAR-10.py

--- src/06_CIFAR-10.py
+++ src/06_CIFAR-10.py
@@ -7,7 +7,243 @@ import tensorflow as tf
 from sklearn.metrics import confusion_matrix
 from datetime import timedelta
 
+import cifar10
+from cifar10 import img_size, num_channels, num_classes
+
 # Use PrettyTensor to simply Neural Network construction
-import prettytensor as pt
+# import prettytensor as pt
 
 print('TensorFlow version: {}'.format(tf.__version__))
+
+# Load Data
+cifar10.maybe_download_and_extract()
+
+class_names = cifar10.load_class_names()
+print('CIFAR-10 class names: {}'.format(class_names))
+
+images_train, cls_train, labels_train = cifar10.load_training_data()
+images_test, cls_test, labels_test = cifar10.load_test_data()
+
+print("Size of:")
+print("- Training-set:\t\t{}".format(len(images_train)))
+print("- Test-set:\t\t{}".format(len(images_test)))
+
+# Data Dimensions
+img_size_cropped = 24
+
+# Helper-function for plotting images
+def plot_images(images_, cls_true_, cls_pred=None, smooth=True):
+    assert len(images_) == len(cls_true_) == 9
+
+    # Create figure with sub-plots.
+    fig, axes = plt.subplots(3, 3)
+
+    # Adjust vertical spacing if we need to print ensemble and best-net.
+    if cls_pred  is None:
+        hspace = 0.3
+    else:
+        hspace=0.6
+    fig.subplots_adjust(hspace=hspace, wspace=0.3)
+
+    for i, ax in enumerate(axes.flat):
+        # Interpolation type.
+        if smooth:
+            interpolation = 'spline16'
+        else:
+            interpolation = 'nearest'
+
+        # Plot image.
+        ax.imshow(images_[i, :, :, :], interpolation=interpolation)
+
+        # Nmae of the true class.
+        cls_true_name = class_names[cls_true_[i]]
+
+        # Show true and predicted classes.
+        if cls_pred is None:
+            xlabel = "True: {0}".format(cls_true_name)
+        else:
+            # Name of the predicted class.
+            cls_pred_name = class_names[cls_pred[i]]
+
+            xlabel = "True: {0}\nPred: {1}".format(cls_true_name, cls_pred_name)
+
+        # Show the classes as the label on the x-axis.
+        ax.set_xlabel(xlabel)
+
+        # Remove ticks from the plot.
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+    # Ensure the plot is shown correctly with multiple plots.
+    # in a single Notebook cell.
+    plt.show()
+
+# Plot a few images to see if data is correct
+# Get the first images fro mthe test-set.
+images = images_test[0:9]
+
+# Get the true classes for those images.
+cls_true = cls_test[0:9]
+
+# Plot the images and labels using our helper-function above.
+plot_images(images_=images, cls_true_=cls_true, smooth=False)
+plot_images(images_=images, cls_true_=cls_true, smooth=True)
+
+# TensorFlow Graph
+# Placeholder variables
+x = tf.placeholder(tf.float32, shape=[None, img_size, img_size, num_channels], name='x')
+y_true = tf.placeholder(tf.float32, shape=[None, num_classes], name='y_true')
+y_true_cls = tf.argmax(y_true, dimension=1)
+
+# Helper-function for creating Pre-Processing
+def pre_process_image(image, training):
+    # This function takes a single iamge as input,
+    # and a boolean whether to build the training or testing graph.
+
+    if training:
+        # For training, add the following to the TensorFlow graph.
+
+        # Randomly crop the input image.
+        image = tf.random_crop(image, size=[img_size_cropped, img_size_cropped, num_channels])
+
+        # Randomly flip the image horizontally.
+        image = tf.image.random_flip_left_right(image)
+
+        # Randomly adust hue, contrast and saturation.
+        image = tf.image.random_hue(image, max_delta=0.05)
+        image = tf.image.random_contrast(image, lower=0.3, upper=1.0)
+        image = tf.image.random_brightness(image, max_delta=0.2)
+        image = tf.image.random_saturation(image, lower=0.0, upper=2.0)
+
+        # Some of these functions may overflow and result in pixel
+        # values beyond the [0, 1] range. It is unclear from the
+        # documentation of TensorFlow 0.10.0rc0 whether this is
+        # intended. A simple solution is to limit the range.
+
+        # Limit the image pixels between [0, 1] in case of overflow.
+        image = tf.minimum(image, 1.0)
+        image = tf.maximum(image, 0.0)
+    else:
+        # For test, add the following to the TensorFlow graph.
+
+        # Crop the input image around the centre so it is the same
+        # size as images taht are randomly cropped during training.
+        image = tf.image.resize_image_with_crop_or_pad(image,
+                                                       target_height=img_size_cropped,
+                                                       target_width=img_size_cropped)
+
+    return image
+
+def pre_process(images_, training):
+    # Use TensorFlow to loop over all the input images and call
+    # the function above which takes a single image as input.
+    images_ = tf.map_fn(lambda image: pre_process_image(image, training), images_)
+
+    return images_
+
+distorted_images = pre_process(images_=x, training=True)
+
+# Helper-function for creating Main Processing
+# Helper-function for creating new variables
+def new_weights(shape):
+    return tf.Variable(tf.truncated_normal(shape, stddev=0.05))
+
+def new_biases(length):
+    return tf.Variable(tf.constant(0.05, shape=[length]))
+
+# Helper-function for creating a new Convolutional Layer
+def new_conv_layer(input_,                          # The previous layer.
+                   num_input_channels,              # Num. channels in prev. layer.
+                   filter_size,                     # Width and height of each filter.
+                   num_filters,                     # Number of filters.
+                   use_pooling=True):               # Use 2x2 max-pooling.
+    # Shape of the filter-weights for the convolution.
+    # This format is determined by the TensorFlow API.
+    shape = [filter_size, filter_size, num_input_channels, num_filters]
+
+    # Create new weights aka. filters with the given shape.
+    weights = new_weights(shape=shape)
+
+    # Create new biases, one for each filter.
+    biases = new_biases(length=num_filters)
+
+    # Create the TensorFlow operation for convolution.
+    # Note the strides are set to 1 in all dimension.
+    # The first and last stride must always be 1,
+    # because the first is for the image-numer and
+    # the last is for the input-channel.
+    # But e.g. strides=[1, 2, 2, 1] would mean that the filter
+    # is moved 2 pixels across the x- and y-axis of the image.
+    # The padding is set to 'SAME' which means the input image
+    # is padded with zeroses so teh size of the output is the same.
+    layer = tf.nn.conv2d(input=input_,
+                         filter=weights,
+                         strides=[1, 1, 1, 1],
+                         padding='SAME')
+
+    # Add the biases to the resutls of the convolution.
+    # A bias-value is added to each filter-channel.
+    layer += biases
+
+    # use pooling to down-sample the image resolution?
+    if use_pooling:
+        # This is 2x2 max-pooling, which mean taht we
+        # consider 2x2 windows and select the largest value
+        # in each window. Then we move 2 pixels to the next window.
+        layer = tf.nn.max_pool(value=layer,
+                               ksize=[1, 2, 2, 1],
+                               strides=[1, 2, 2, 1],
+                               padding='SAME')
+
+        # Rectified Linear unit (ReLU).
+        # It calculates max(x, 0) for each input pixel x.
+        # This add some non-linearity to the formula and allows us
+        # to learn more complicated functions.
+        layer = tf.nn.relu(layer)
+
+        # Note that ReLU is normally executed before the pooling,
+        # but since relu(max_pool(x)) == max_pool(relu(x)) we can
+        # save 75% of the relu-operations by max-pooling first.
+
+        # We return both the resulting layer and the filter-weights
+        # because we will plot the weights layer.
+        return layer, weights
+
+# Helper-function for flattening a layer
+def flatten_layer(layer):
+    # Get the shape of the input layer.
+    layer_shape = layer.get_shape()
+
+    # The shape of the input layer is assumed to be:
+    # layer_shape == [num_images, img_height, img_width, num_channels]
+
+    # The number of features is: img_height * img_width * num_channels
+    # We can use a function from TensorFlwo to calculate this.
+    num_features = layer_shape[1:4].num_elements()
+
+    # Reshape the layer to [num_images, num_features].
+    # Note that we just set the size of the second dimension
+    # to num_features and the size of the first dimension to -1
+    # wihich means the size in that dimension is calculated
+    # so the total size of the tensor is unchanged from the reshaping.
+    layer_flat = tf.reshape(layer, [-1, num_features])
+
+    # The shape of the flattened layer is now:
+    # [num_images, img_height * img_width * num_channels]
+
+    # Return both the flattened layer and the number of features.
+    return layer_flat, num_features
+
+# Helper-function for creating a new Fully-Connected Layer
+
+
+
+
+
+
+
+
+
+
+
+
