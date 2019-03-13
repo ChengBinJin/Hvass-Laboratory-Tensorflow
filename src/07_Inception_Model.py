92f35055489902c4e71140dc238758311a96c2a3
chengbin <sbkim0407@gmail.com>
2019-03-13 23:34:07 +0900

    start to implement 07_Inception_Model.py

---
+++ src/07_Inception_Model.py
@@ -0,0 +1,42 @@
+# Imports
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+# Functions and classes for loading and using the Inception model.
+# import inception
+
+print("TensorFlow version: {}".format(tf.__version__))
+
+# Download the Inception Model
+
+# Load the Inceptin Model
+
+# Helper-function for classifying and plotting images
+
+# Panda
+
+# Interpretation of Classification Scores
+
+# Parrot (Original Image)
+
+# Parrot (Resized Image)
+
+# Parrot (Cropped Image, Top)
+
+# Parrot (Cropped Image, Middle)
+
+# Parrot (Cropped Image, Bottom)
+
+# Parrot (Paded Image)
+
+# Elon Musk (299 x 299 pixels)
+
+# Elon Musk (100 x 100 pixels)
+
+# Willy Wonka (Gene Wilder)
+
+# Willy wonka (Johnny Depp)
+
+# Close TensorFlow Session
