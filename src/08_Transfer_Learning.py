16992dcc7376f8ba9bd2ee7a968afc675c704f18
chengbin <sbkim0407@gmail.com>
2019-04-23 10:07:30 +0900

    Finished 08_Transfer_Learning.py

--- src/08_Transfer_Learning.py
+++ src/08_Transfer_Learning.py
@@ -206,34 +206,228 @@ loss = tf.nn.sigmoid_cross_entropy_with_logits(logits=y_pred, labels=y_true)
 global_step = tf.get_variable(initializer=0, trainable=False, name='global_step')
 optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(loss, global_step)
 
-
-
-
 # Classification Accuracy
+y_pred_cls = tf.math.argmax(input=y_pred, axis=1)
+correct_prediction = tf.math.equal(x=y_pred_cls, y=y_true_cls)
+accuracy = tf.math.reduce_mean(input_tensor=tf.dtypes.cast(x=correct_prediction, dtype=tf.float32))
 
 # TensorFlow Run
 # Create TensorFlow Session
+session = tf.Session()
 
 # Initialize Variables
+session.run(tf.global_variables_initializer())
 
 # Helper-function to get a random training-batch
+train_batch_size = 64
+
+def random_batch():
+    # Number of images (transfer-values) in the training-set.
+    num_images = len(transfer_values_train)
+
+    # Create a random index.
+    idx = np.random.choice(num_images,
+                           size=train_batch_size,
+                           replace=False)
+
+    # Use the random index to select random x and y-values.
+    # We use the transfer-values instead of images as x-values.
+    x_batch = transfer_values_train[idx]
+    y_batch = labels_train[idx]
+
+    return x_batch, y_batch
 
 # Helper-function to perform optimization
+def optimize(num_iterations):
+    # Start-time used for printing time-usage below.
+    start_time = time.time()
+
+    for i in range(num_iterations):
+        # Get a batch of training examples.
+        # x_batch now holds a batch of images (transfer-values) and
+        # y_true_batch are the true labels for those images.
+        x_batch, y_true_batch = random_batch()
+
+        # Put the batch into a dict with the proper names
+        # for placeholder variables in the Tensorflow graph.
+        feed_dict_train = {x: x_batch,
+                           y_true: y_true_batch}
+
+        # Run the optimizer using this batch of training data.
+        # TensorFlow assigns the variables in feed_dict_train
+        # to the placeholder variables and then runs the optimizer.
+        # We also want to retrieve the global_step counter.
+        i_global, _ = session.run([global_step, optimizer],
+                                  feed_dict=feed_dict_train)
+
+        # Print status to screen every 100 iterations (and last).
+        if (i_global % 100 == 0) or (i == num_iterations - 1):
+            # Caqlculate the accuracy on the training-batch.
+            batch_acc = session.run(accuracy,
+                                    feed_dict=feed_dict_train)
+
+            # Print status.
+            msg = "Global Step: {0:>6}, Training Batch Accuracy: {1:>6.1%}"
+            print(msg.format(i_global, batch_acc))
+
+    # End time.
+    end_time = time.time()
+
+    # Differene between start and end-times.
+    time_dif = end_time - start_time
+
+    # Print the time-usage
+    print("Time usage: " + str(timedelta(seconds=int(round(time_dif)))))
 
 # Helper-function for Showing Results
 # Helper-function to plot example errors
+def plot_example_errors(cls_pred, correct):
+    # This function is called from print_test_accuracy() below.
+
+    # cls_pred is an array of the predicted class-number for
+    # all images in the  test-set.
+
+    # correct is a boolean array whether the predicted class
+    # is equal to the true class for each image in the test-set.
+
+    # Negate the boolean array.
+    incorrect = (correct == False)
+
+    # Get the iamges from the test-set that have been
+    # incorrectly classified.
+    images = images_test[incorrect]
+
+    # Get the predicted classes for those images.
+    cls_pred = cls_pred[incorrect]
+
+    # Get the true classes for those images.
+    cls_true = cls_test[incorrect]
+
+    n = min(9, len(images))
+
+    # Plot the first n images.
+    plot_images(images=images[:n],
+                cls_true=cls_true[:n],
+                cls_pred=cls_pred[:n])
 
 # Helper-function to plot confusion matrix
+# Import a function from sklearn to calculate the confusion-matrix.
+from sklearn.metrics import confusion_matrix
+
+def plot_confusion_matrix(cls_pred):
+    # This is called from print_test_accuracy() below.
+
+    # cls_pred is an array of the predicted class-number for
+    # all images in the test-set.
+
+    # Get the confusion matrix using sklearn.
+    cm = confusion_matrix(y_true=cls_test,  # True class for test-set.
+                          y_pred=cls_pred)  # Predicted class.
+
+    # Print the confusion matrix as text.
+    for i in range(num_classes):
+        # Append the class-name to each line.
+        class_name = "({}) {}".format(i, class_names[i])
+        print(cm[i, :], class_name)
+
+    # Print the class-numbers for easy reference
+    class_numbers = [" ({0})".format(i) for i in range(num_classes)]
+    print("".join(class_numbers))
 
 # Helper-functions for calculating classifications
+# Split the data-set in batches of this size to limit RAM usage.
+batch_size = 256
+
+def predict_cls(transfer_values, labels, cls_true):
+    # Number of images
+    num_images = len(transfer_values)
+
+    # Allocate an array for the predicted classes which
+    # will be calcualted in batches and filled into this array.
+    cls_pred = np.zeros(shape=num_images, dtype=np.int)
+
+    # Now calculate the predicted classes for the batches.
+    # We will just iterate through all the batches.
+    # There might be a more clever and Pythonic way of doing this.
+
+    # The starting index for the next batch is denoted in.
+    i = 0
+
+    while i < num_images:
+        # The ending index for the next batch is denoted j.
+        j = min(i + batch_size, num_images)
+
+        # Create a feed-dict with the images and labels
+        # between index i and j.
+        feed_dict = {x: transfer_values[i:j],
+                     y_true: labels[i:j]}
+
+        # Calculate teh predicted class using TensorFlow.
+        cls_pred[i:j] = session.run(y_pred_cls, feed_dict=feed_dict)
+
+        # Set the start-index for the next batch to the
+        # end-index of the current batch.
+        i = j
+
+    # Crate a boolean array whether each iamge is correctly classified.
+    correct = (cls_true == cls_pred)
+
+    return correct, cls_pred
+
+def predict_cls_test():
+    return predict_cls(transfer_values=transfer_values_test,
+                       labels=labels_test,
+                       cls_true=cls_test)
 
 # Helper-function for calculating classification accuracy
+def classification_accuracy(correct):
+    # When averaging a boolean array. False means 0 and True means 1.
+    # So we are calculating: number of True / len(correct) which is
+    # the same as the classification accuracy.
+
+    # Return the classification accuracy
+    # and the number of correct classifications.
+    return correct.mean(), correct.sum()
 
 # Helper-function for showing the classification accuracy
+def print_test_accuracy(show_example_errors=False,
+                        show_confusion_matrix=False):
+    # For all the images in the test-set,
+    # calculate the predicted classes and whether they are correct.
+    correct, cls_pred = predict_cls_test()
+
+    # Classification accuracy and teh number of correct classification.
+    acc, num_correct = classification_accuracy(correct)
+
+    # Number of images being classified.
+    num_images = len(correct)
+
+    # Print the accuracy.
+    msg = "Accuracy on Test-set: {0:.1%} ({1} / {2})"
+    print(msg.format(acc, num_correct, num_images))
+
+    # Plot some examples of mis-classification, if desired.
+    if show_example_errors:
+        print("Example errors:")
+        plot_example_errors(cls_pred=cls_pred, correct=correct)
+
+    # Plot the confusion matrix, if desired.
+    if show_confusion_matrix:
+        print("Confusion Matrix:")
+        plot_confusion_matrix(cls_pred=cls_pred)
 
 # Results
 # Performance before any optimization
+print("Performance before any optimization!")
+print_test_accuracy(show_example_errors=False,
+                    show_confusion_matrix=False)
 
 # Performance after 10,000 optimization iterations
+print("Performance after 10,000 optimization iterations!")
+optimize(num_iterations=10000)
+print_test_accuracy(show_example_errors=True,
+                    show_confusion_matrix=True)
 
 # Close TensorFlow Session
+model.close()
+session.close()
