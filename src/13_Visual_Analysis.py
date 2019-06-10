4a0879cd8624891ce320089b926fc96601612285
bobby <sbkim0407@gmail.com>
2019-06-10 23:46:44 +0900

    add 13_Visual_Analysis.py

---
+++ src/13_Visual_Analysis.py
@@ -0,0 +1,14 @@
+# TensorFlow Tutorial #13
+# Visual Analysis
+# Introduction
+# Flowchart
+
+# Imports
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+# Functions and classes for loading and using the Inception model.
+import inception
+
+print('TensorFlow Version: {}'.format(tf.__version__))
