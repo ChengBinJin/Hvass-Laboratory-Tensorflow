147623ec5a557c03f5dbc24419d8cc65359f708c
chengbin <sbkim0407@gmail.com>
2019-07-19 10:56:10 +0900

    working reinforcement_learning.py

--- src/reinforcement_learning.py
+++ src/reinforcement_learning.py
@@ -873,13 +873,6 @@ class ReplayMemory:
                          np.mean(self.q_values),
                          np.max(self.q_values)))
 
-        # Print statistics for the Q-values after they were updated
-        # in update_all_q_values().
-        msg = "\tQ-values After, Min: {0:5.2f}, Mean: {1;5.2f}, Max: {2:5.2f}"
-        print(msg.format(np.min(self.q_values),
-                         np.mean(self.q_values),
-                         np.max(self.q_values)))
-
         # Print statistics for the difference in Q-values before and
         # after the update in update_all_q_values().
         q_dif = self.q_values - self.q_values_old
@@ -913,7 +906,7 @@ class ReplayMemory:
 ##################################################################################
 
 
-class LinearControlSingla:
+class LinearControlSignal:
     """
     A control signal that changes linearly over time.
 
@@ -925,3 +918,184 @@ class LinearControlSingla:
     want the control signals to use a state-counter for the
     game-environment. So it is eaiser to make this in Python.
     """
+
+    def __init__(self, start_value, end_value, num_iterations, repeat=False):
+        """
+        Create a new object.
+
+        :param start_value:
+            Start-value for the control signal.
+
+        :param end_value:
+            End-value for the control signal.
+
+        :param num_iterations:
+            Number of iterations it takes to reach the end_value
+            from the start_value.
+
+        :param repeat:
+            Boolean whether to reset the control signal back to the start_value
+            after  the end_value has been reached.
+        """
+
+        # Store arguments in this object.
+        self.start_value = start_value
+        self.end_value = end_value
+        self.num_iterations = num_iterations
+        self.repeat = repeat
+
+        # Calculate the linear coefficient.
+        self._coefficient = (end_value - start_value) / num_iterations
+
+    def get_value(self, iteration):
+        """Get the value of the control signal for the given iteration."""
+
+        if self.repeat:
+            iteration %= self.num_iterations
+
+        if iteration < self.num_iterations:
+            value = iteration * self._coefficient + self.start_value
+        else:
+            value = self.end_value
+
+        return value
+
+##################################################################################
+
+
+class EpsilonGreedy:
+    """
+    The epsilon-greedy policy either takes a random action with
+    probability epsilon, or it takes the action for the highest
+    Q-value.
+
+    If epsilon is 1.0 then the actions are always random.
+    If epsilon is 0.0 then the actions are always argmax for the Q-values.
+
+    Epsilon is typically decreased linearly from 1.0 to 0.1
+    and this is also implemented in this class.
+
+    During testing, epsilon is usually chosen lower, e.g. 0.05 or 0.01
+    """
+
+    def __init__(self, num_actions,
+                 epsilon_testing=0.05,
+                 num_iterations=1e6,
+                 start_value=1.0, end_value=0.1,
+                 repeat=False):
+        """
+
+        :param num_actions:
+            Number of possible actions in the game-environment.
+
+        :param epsilon_testing:
+            Epsilon-value when testing.
+
+        :param num_iterations:
+            Number of training iterations required to linearly
+            decrease epsilon from start_value to end_value.
+
+        :param start_value:
+            Starting value for linearly decreasing epsilon.
+
+        :param end_value:
+            Ending value for lnearly decreasing epsilon.
+
+        :param repeat:
+            Boolean whether to repeat and restart the linear decrease
+            when the end_value is reached, or only do it once and then
+            output the end_value forever after.
+        """
+
+        # Store parameters.
+        self.num_actions = num_actions
+        self.epsilon_testing = epsilon_testing
+
+        # Create a control signal for linearly decreasing epsilon.
+        self.epsilon_linear = LinearControlSignal(num_iterations=num_iterations,
+                                                  start_value=start_value,
+                                                  end_value=end_value,
+                                                  repeat=repeat)
+
+    def get_epsilon(self, iteration, training):
+        """
+        Return the epsilon for the given iterations.
+        If training=True then epsilon is linearly decreased,
+        otherwise epsilon is a fixed number.
+        """
+
+        if training:
+            epsilon = self.epsilon_linear.get_value(iteration=iteration)
+        else:
+            epsilon = self.epsilon_testing
+
+        return epsilon
+
+    def get_action(self, q_values, iteration, training):
+        """
+        Use the epsilon-greedy policy to select an action.
+
+        :param q_values:
+            These are the Q-values that are estimated by the Neural Network
+            for the current state of the game-environment.
+
+        :param iteartion:
+            This is an iteration counter. Here we use the number of states
+            that has been processed in the game-environment.
+
+        :param training:
+            Boolean whether we are training or testing the
+            Reinforcement Learnign agent.
+
+        :return:
+            action (integer), epsilon (float)
+        """
+
+        epsilon = self.get_epsilon(iteration=iteration, training=training)
+
+        # With the probability epsilon.
+        if np.random.random() < epsilon:
+            # Select a random action.
+            action = np.random.randint(low=0, high=self.num_actions)
+        else:
+            # Otherwise select the action that has the highest Q-values.
+            action = np.argmax(q_values)
+
+        return action, epsilon
+
+##################################################################################
+
+
+class NeuralNetwork:
+    """
+    Creates a Neural Network for Reinforcement Learning (Q-Learning).
+
+    Functions are provided for estimating Q-values from states of the
+    game-environment, and for optimizing the Neural network so it becomes
+    better at estimating the Q-values.
+    """
+
+    def __init__(self, num_actions, replay_memory):
+        """
+        :param num_actions:
+            Number of discrete actions for the game-environment.
+
+        :param replay_memory:
+            Object-instance of the ReplayMemory-class.
+        """
+
+        # Replay-memory used for sampling random batches.
+        self.replay_memory = replay_memory
+
+        # Path for saving/restoring checkpionts.
+        self.checkpoint_path = os.path.join(checkpoint_dir, "checkpoint")
+
+        # Placeholder variable for inputting states into the Neural Network.
+        # A state is a multi-dimensional array holding image-frames from
+        # the game-environment.
+        self.x = tf.placeholder(dtype=tf.float32, shape=[None] + state_shape, name='x')
+
+        # Placeholder variable for inputting the learning-rate to the optimizer.
+        self.learning_rate = tf.placeholder(dtype=tf.float32, shape=[])
+
+        
